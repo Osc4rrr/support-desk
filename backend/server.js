d839237aa18ead704c99f01e15eaf293cba1a4a9
Oscar Diaz <os.diazl@alumnos.duoc.cl>
2022-07-17 20:53:06 -0400

    fontend added

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,27 @@
-console.log('Server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const { connect } = require('mongoose')
+const PORT = process.env.PORT || 8000
+const app = express() 
 
+//Connect to database
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log('Server Started on port ' + PORT))
